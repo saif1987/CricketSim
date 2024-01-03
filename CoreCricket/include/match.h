be25a6b235e95f41f0fc37d80a5864026f1ec202
saif1987 <md.saif05@gmail.com>
2024-01-03 00:20:09 -0500

    Match added

--- CoreCricket/include/match.h
+++ CoreCricket/include/match.h
@@ -10,39 +10,42 @@
 #include "bowler.h"
 #include "innings.h"
 
-class Player;
-class Team;
-
-
-class Match{
-public:
-    Team T1,T2;
-private:
-
-
-};
-
-
-class Team{
-public:
-    std::string Name;
-    Player players[11];
-    Team(std::string name, std::vector< std::string > playerNames){ 
-        Name = name;
-        if(playerNames.size()!=11)
-            throw std::invalid_argument("Number of Player needs to be 11");
-        int i=0;
-        for(auto pName: playerNames){
-            players[i++].Name = name;
-        }    
-    };
-};
-
 class Player{
 public: 
     std::string Name;
     Player(){};
     Player(std::string name){
         Name = name;
-    }
+    };
 };
+
+class Team{
+public:
+    std::string Name;
+    std::vector<Player> players;
+    Team(){};
+    Team(std::string name, std::vector< std::string > playerNames){ 
+        Name = name;
+        if(playerNames.size()!=11)
+            throw std::invalid_argument("Number of Player needs to be 11");
+
+        for(auto i: playerNames)
+        {
+            players.push_back(Player(i));
+        } 
+    };
+};
+
+
+class Match{
+public:
+    Team T1;
+    Team T2;
+    Match(){};
+    Match(Team t1, Team t2){
+        T1 = t1;
+        T2 = t2;
+    }
+private:
+};
+
--- main.cpp
+++ main.cpp
@@ -1,8 +1,21 @@
 #include <iostream>
-#include "play.h"
+#include "match.h"
 
 int main()
 {
     std::cout<<"Hello"<<std::endl;
+
+    Team t1 = Team("Team1",{
+        "T1Player1","T1Player2","T1Player3","T1Player4",
+        "T1Player5","T1Player6","T1Player7","T1Player8",
+        "T1Player9","T1Player10","T1Player11"});
+
+    Team t2 = Team("Team2",{
+        "T2Player1","T2Player2","T2Player3","T2Player4",
+        "T2Player5","T2Player6","T2Player7","T2Player8",
+        "T2Player9","T2Player10","T2Player11"});
+
+    Match match = Match(t1,t2);
+
     return 0;
 }
